f2f46c3812fecae5480fd7593f3ed424ffdfb2bf
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-03-25 21:40:51 +1300

    update router set up and default address

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
 	. "github.com/ElPlaso/blundle-go/shared"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -14,9 +16,19 @@ func main() {
 		log.Printf("Error loading .env file: %s", envErr)
 	}
 
-	router := gin.Default()
+	router := gin.New()
+	router.Use(cors.Default())
 	router.GET("/daily-puzzle", GetDailyPuzzle)
 	router.GET("/add-puzzle", AddPuzzle)
 
-	router.Run("localhost:8080")
+	envPort, envPortFound := os.LookupEnv("PORT")
+
+	address := ""
+	if envPortFound {
+		address = "0.0.0.0:" + envPort // For render
+	} else {
+		address = "localhost:8080"
+	}
+
+	router.Run(address)
 }
